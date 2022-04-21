9a7255579f39c1aef0f7600c2b451cd4e24dc61e
jawad <jawadusaid11@gmail.com>
2022-04-21 13:20:00 +0530

    SingleTableInheritance

---
+++ SingleTableInteritance/src/com/cg/jpastart/entities/InheritanceTest.java
@@ -0,0 +1,37 @@
+package com.cg.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one employee
+		Employee employee = new Employee();
+		employee.setName("Mansi");
+		employee.setSalary(20000);
+		em.persist(employee);
+
+		
+		//create one manager
+		Manager manager = new Manager();
+		manager.setName("Shubro");
+		manager.setSalary(400000);
+		manager.setDepartmentName("Training");
+		em.persist(manager);
+		
+		
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added one employee and manager to database.");
+		em.close();
+		factory.close();
+	}
+}
